bd651ef55be1a6fd4b8209424aebca4ee9fc18af
Ricky Tobing <ricky.tobing@telos.com>
2015-01-20 10:42:31 -0500

    Breaking changes: * Upgrade to latest gradle build configs * GoogleDrive is no workie :(

--- Providers/Dropbox/src/androidTest/java/com/dropbox/client2/MockAccount.java
+++
@@ -1,7 +0,0 @@
-package com.dropbox.client2;
-
-public class MockAccount extends DropboxAPI.Account {
-    public MockAccount(String country, String displayName, long uid, String referralLink, long quota, long quotaNormal, long quotaShared) {
-        super(country, displayName, uid, referralLink, quota, quotaNormal, quotaShared);
-    }
-}
